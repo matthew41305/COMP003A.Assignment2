b45882fcf88246e48a1c33042fd80da1fd75ab9e
matth <matth@DESKTOP-3D0678T>
2024-01-25 20:14:29 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Author: Matthew Hudson
+ * Course: COMP003A
+ * Purpose: Programming Basics
+ */
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // String Section
+            Console.Title = "COMP-003A - Assignment 2";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            string firstName = Console.ReadLine();
+            string middleName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string inputAge = Console.ReadLine();
+            int yearBorn = 2024 - Convert.ToInt32(inputAge);
+            Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}. ");
+
+            
+
+        }
+    }
+}
